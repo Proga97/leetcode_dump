a531eafe286cb477eeba8a6eccafa82315b969f5
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-03-16 23:47:48 -0400

    Time: 0 ms (100%), Space: 19.4 MB (58.46%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        map = {')' : '(', '}': '{' , ']' :'[' }
+        stack = []
+
+        for n in s:
+            if n in map:
+                if stack and stack[-1] == map[n]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(n)
+        return len(stack) == 0 
