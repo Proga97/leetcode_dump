2607a033fe97f42b4307407f40e9b4413a43ec92
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-03-16 23:29:44 -0400

    Time: 0 ms (100%), Space: 19.4 MB (18.54%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -0,0 +1,43 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+         # Recursive method for inserting a value in the BST
+        # DFS
+        if not root:
+            return
+        # root.left, root.right = root.right, root.left
+
+        # self.invertTree(root.left)
+        # self.invertTree(root.right)
+        # return root
+       
+        # DFS Stack
+        # stack = [root]
+        # while stack:
+        #     node = stack.pop()
+        #     node.left, node.right = node.right, node.left
+            
+        #     if node.left:
+        #         stack.append(node.left)
+        #     if node.right:
+        #         stack.append(node.right)
+        # return root
+
+        # BFS Stack
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            node.left, node.right = node.right, node.left
+            
+            if node.right:
+                queue.append(node.right)
+            if node.left:
+                queue.append(node.left)
+        
+        return root
