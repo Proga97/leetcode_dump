dfcb7b1260c24fa14411564d2bbf341de036c382
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-05-27 00:14:53 +0530

    Time: 18 ms (24.45%), Space: 23.9 MB (18.35%) - LeetHub - Time: O(NM), Space: O(NM)

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,15 @@
-from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if len(strs) == 0:
-            return [""]
-        dic= defaultdict(list)
-        for s in strs:
-            count =  [0] * 26
-            for c in s:
-                count[ ord(c) - ord('a') ] += 1
-            count = tuple(count)
-            dic[count].append(s)
-       
-        return list(dic.values())
- 
-
-
+        hashmap = defaultdict(list)
+        for word in strs:
+            count = [0] * 26
+            for s in word:
+                count[ord(s) - ord('a')] += 1
+            hashmap[tuple(count)].append(word)
+        return list(hashmap.values())
         
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
