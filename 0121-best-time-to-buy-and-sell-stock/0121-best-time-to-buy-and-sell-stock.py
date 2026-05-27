dbc72bcb93e2a0861fbb8c80bac0e7f6b589bc87
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-05-28 00:30:31 +0530

    Time: 60 ms (30.84%), Space: 28.5 MB (76.04%) - LeetHub - Time: O(n), Space: O(1)

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        lowest = float('inf')
-        for price in prices:
-            if lowest > price:
-                lowest = price
-            # print(lowest,price, profit)
-            profit = max(profit,price-lowest)
-        return profit
+        if len(prices) <= 1:
+            return 0
+        low = prices[0]
+        max_profit = 0
+        for i in range(1,len(prices)):
+            price = prices[i]
+            low = min(low, price)
+            if price > low:
+                max_profit = max(price - low, max_profit)
+        return max_profit
+            
 
-
-
-
-                
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
