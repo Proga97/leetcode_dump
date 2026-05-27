91d7ec56a92f823531e9fc7986578ddd499ba15c
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-05-27 21:24:18 +0530

    Time: 4 ms (89.82%), Space: 19.7 MB (56.36%) - LeetHub - Time: O(?), Space: O(?)

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,15 +3,33 @@ class Solution:
         if len(s) <= 1:
             return True
         s = s.lower()
-        res = []
-        for a in s:
-            if a.isalnum():
-                res.append(a)
-        # print(res)
-        for i in range(len(res)//2):
-            # print(res[i],res[-i])
-            if res[i] != res [-i-1]:
+
+        ## Two pointers
+        i = 0
+        j = len(s) - 1
+
+        while j > i:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            # print(s[i], s[j])
+            if s[i] != s[j]:
                 return False
+            i += 1
+            j -= 1
+        return True
+        
+        ### comparing ends
+        # res = []
+        # for a in s:
+        #     if a.isalnum():
+        #         res.append(a)
+        # # print(res)
+        # for i in range(len(res)//2):
+        #     # print(res[i],res[-i])
+        #     if res[i] != res [-i-1]:
+        #         return False
         return True
         
 
