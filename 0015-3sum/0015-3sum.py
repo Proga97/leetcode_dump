0eb7b48d67405a48e6572b58abf7632c96ae9c7f
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-05-28 00:07:41 +0530

    Time: 479 ms (92.41%), Space: 22.2 MB (81.03%) - LeetHub - Time: O(?), Space: O(?)

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -16,6 +16,8 @@ class Solution:
                         r -= 1
                         while nums[l] == nums[l - 1] and l < r:
                             l += 1
+                        while nums[r] == nums[r + 1] and l < r:
+                            r -= 1
                     elif total < 0:
                         l += 1
                     else: 
