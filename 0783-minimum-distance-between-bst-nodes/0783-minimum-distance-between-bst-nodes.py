cb5fbf32aeb19d7529a2d7f4a5f039c99f5f8ef8
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-06-03 19:51:19 +0530

    Time: 0 ms (100%), Space: 19.5 MB (10.81%) - LeetHub - Time: O(?), Space: O(?)

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
@@ -5,28 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return
-        if not root.right and not root.left:
-            return
+    def __init__(self):
+        self.prev = None
+        self.diff = float('inf')
+    def trans(self,root):
+        if not root: 
+            return 0
+        self.trans(root.left)
 
-        arr = []
-
-        def inOrder(node):
-            if not node:
-                return
-            
-            inOrder(node.left)
-            arr.append(node.val)
-            inOrder(node.right)
-        inOrder(root)
-        print(arr)
-        minD = float("inf")
-        for i in range(1,len(arr)):
-            minD = min(minD,arr[i] - arr[i-1]) 
-        return minD
-            
-
-
+        # self.nodes.append(root.val)
+        if self.prev:
+            self.diff = min(self.diff, abs(root.val - self.prev.val))
+        self.prev = root
+        self.trans(root.right)
         
+
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        self.trans(root)
+        # print(self.nodes)
+        # diff = float('inf')
+        # for n in range(1,len(self.nodes)):
+            # diff = min(diff, abs(self.nodes[n-1] - self.nodes[n]))
+            # print(diff)
+        return self.diff
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
