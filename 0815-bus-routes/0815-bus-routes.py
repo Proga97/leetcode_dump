72ee9ce39933743a56edaaa82894ef2c6af83d9c
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-06-08 02:45:21 +0530

    Time: 840 ms (22.9%), Space: 41.8 MB (83.73%) - LeetHub - Time: O(?), Space: O(?)

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -2,42 +2,44 @@ class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
         if source == target:
             return 0
-
-        bus_stops = defaultdict(list)
-        for i, bus in enumerate(routes):
-            for stop in bus:
-                bus_stops[stop].append(i)
-        # print(bus_stops)
-
-        queue = collections.deque()
-        # print(bus_stops[source])
-        queue.append(bus_stops[source])
-        # print(queue)
+        stops_map = defaultdict(list)
+        for bus,stops in enumerate(routes):
+            for stop in stops:
+                stops_map[stop].append(bus)
+        # print(stops_map)
+        que = deque()
+        seen = set()
+        que.append(stops_map[source])
+        for buses in stops_map[source]:
+            seen.add(buses)
         buses_count = 1
-        visited = set()
-        while queue:
-            buses = queue.popleft()
-            # print(buses)
-            new_buses = []
-            for bus in buses:
-                # print(bus,"bus")
-                
-                if bus not in visited:
-                    visited.add(bus)
-                    # print(bus)
-                    for stop in routes[bus]:
-                        # print("stop",stop)
-                        if stop == target:
-                            return buses_count
-                        for bus in bus_stops[stop]:
-                            # print("next bus",bus)
-                            if bus not in visited:
-                                new_buses.append(bus)
-                                
-            if new_buses:
-                queue.append(new_buses)
-            buses_count += 1    
+        while que:
+            curr_buses = que.popleft()
+            next_buses = []
+            for curr_bus in curr_buses:
+                for stop in routes[curr_bus]:
+                    # print(que,seen,curr_bus,stop)
+                    if stop == target:
+                        return buses_count
+                    for next_bus in stops_map[stop]:
+                        if next_bus not in seen:
+                            # print("next_bus",next_bus,que,seen,curr_bus,stop)
+                            next_buses.append(next_bus)
+                            seen.add(next_bus)
+            if next_buses:
+                que.append(next_buses)  
+                buses_count += 1             
+
+        return -1   
+
+                        
+
+                    
 
 
 
-        return -1
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
