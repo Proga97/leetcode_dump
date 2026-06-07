c962119e5b5e045677ddd6217fbfffbe9a8020ff
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-06-08 01:44:26 +0530

    Time: 36 ms (67.59%), Space: 28.6 MB (18.52%) - LeetHub - Time: O(?), Space: O(?)

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,32 +1,29 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         n = len(graph)
-        seen = [0] * n
-        safe = []
+        safe = {}
+        res = []
+
         def dfs(node):
-            # print(node)
-            if seen[node] == 1:
-                # print("seen")
-                return True
-            if seen[node] == -1:
-                # print("seen false",seen[node])
-                return False
-            seen[node] = -1
-            if len(graph[node]) == 0:
-                # print("00",graph[node],node)
-                seen[node] = 1
-                return True
-            for i in graph[node]:
-                if not dfs(i):
+            if node in safe:
+                return safe[node]
+            safe[node] = False
+            for nei in graph[node]:
+                if not dfs(nei):
                     return False
-            seen[node] = 1
+            safe[node] = True
             return True
+
+           
 
         for i in range(len(graph)):
             if dfs(i):
-                safe.append(i)
-        # print(safe)
-        return safe
+                res.append(i)
+        return res
                 
 
         
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
