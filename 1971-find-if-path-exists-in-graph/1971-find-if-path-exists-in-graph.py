32149c60f6765ddfbf1b2872523618b30c53b61e
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-06-08 00:57:57 +0530

    Time: 259 ms (80.1%), Space: 92.5 MB (80.18%) - LeetHub - Time: O(?), Space: O(?)

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,13 +1,13 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        seen = set()
+        seen = [False] * n
         route_map = defaultdict(list)
         for i in edges:
             route_map[i[0]].append(i[1])
             route_map[i[1]].append(i[0])
         # print(route_map)
         stack = [source]
-        seen.add(source)
+        seen[source] = True
 
         while stack:
             curr = stack.pop()
@@ -16,11 +16,11 @@ class Solution:
             for i in route_map[curr]:
                 if i == destination:
                     return True
-                if i not in seen:
-                    seen.add(i)
+                if not seen[i]:
+                    seen[i] = True
                     stack.append(i)
 
-        return destination in seen
+        return seen[destination]
 
 
         
