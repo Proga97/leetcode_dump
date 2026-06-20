af563cacdde4856cf26f6308ac35a312e875d1b9
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-06-20 23:39:48 +0530

    Time: 2 ms (20.26%), Space: 19.5 MB (5.06%) - LeetHub - Time: O(n log n), Space: O(1)

---
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
+        total_apples = sum(apple)
+        capacity.sort(reverse = True)
+        capacity_total = 0
+        for i, val in enumerate(capacity):
+            capacity_total += val
+            if capacity_total >= total_apples:
+                return i + 1
+
+        return -1
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
