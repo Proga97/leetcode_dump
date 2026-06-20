113b41f7bbcbc6e04d48a7a03d926e29ad2fcd22
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-06-21 01:20:40 +0530

    Time: 3 ms (84.32%), Space: 21.7 MB (22.91%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -0,0 +1,17 @@
+import heapq
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        intervals = sorted(intervals, key= lambda x: x[0])
+        rooms = []
+        heapq.heappush(rooms,intervals[0][1])
+        for i in range(1, len(intervals)):
+            if rooms[0] <= intervals[i][0]:
+                heapq.heappop(rooms)
+            heapq.heappush(rooms,intervals[i][1])
+        return len(rooms)
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
