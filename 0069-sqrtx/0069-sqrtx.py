0a1f2e8a1a80f7369db668da1cf411f25b695d9e
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-06-22 02:14:48 +0530

    Time: 2 ms (73.96%), Space: 19.3 MB (57.69%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        high = x//2 + 1
+        low = 1
+        while low <= high:
+            mid = (low + high) // 2
+            sq = mid * mid
+            if sq > x:
+                high = mid -1
+            elif sq < x:
+                low = mid + 1
+            else: return mid
+        return high
+
+        
+        
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
