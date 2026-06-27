21b132df50dbebcbeec59ab649fd646d5095d686
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-06-28 04:22:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (49.11%) - LeetHub - Time: O(n), Space: O(1)

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -3,15 +3,9 @@ class Solution:
         seen = set()
 
         # Iterate over each character using a normal for loop
-        for i in range(len(sentence)):
-            # Convert the current character to lowercase
-            currChar = sentence[i].lower()
-            
-            if currChar.isalpha():
-                # Add the character to the set
-                seen.add(currChar)
+        for i in sentence:            
+            seen.add(i)
 
-        # Return true if set size is 26 (total number of alphabets)
         return len(seen) == 26
 
 # Synced seamlessly with LeetHub Pro
