5e0ef8b23b4925e9550746c31b7ae0bf1f4077d3
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-17 11:49:06 -0400

    Time: 0 ms (100%), Space: 19.3 MB (67.33%) - LeetHub - Time: O(m + n), Space: O(m + n)

---
+++ 0739-next-greater-element-i/0739-next-greater-element-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        hashmap = {}
+
+        for n in nums2:
+            while stack and stack[-1] < n:
+                hashmap[stack.pop()] = n
+            stack.append(n)
+        
+        # res = []
+        # for n in nums1:
+        #     x = hashmap.get(n, -1)
+        #     res.append(x)
+        
+        return [hashmap.get(n, -1) for n in nums1]
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
