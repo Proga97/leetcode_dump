f671fba2275e6c34cd3a418820ad032d470c3827
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-20 16:09:23 -0400

    Time: 3 ms (99.56%), Space: 25.5 MB (58.59%) - LeetHub - Time: O(?), Space: O(?)

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -1,20 +1,29 @@
 class KthLargest:
-
+    
     def __init__(self, k: int, nums: List[int]):
-        self.minHeap, self.k = nums, k
-        heapq.heapify(self.minHeap)
-        while len(self.minHeap) > k:
-            heapq.heappop(self.minHeap)        
+        self.heap = []
+        self.k = k
+   
+        for n in nums:
+            heappush(self.heap, n)
+            if len(self.heap) > self.k:
+                heappop(self.heap)
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.minHeap,val)
-        if len(self.minHeap) > self.k:
-            heapq.heappop(self.minHeap) 
-        return self.minHeap[0]   
-        
+        if len(self.heap) >= self.k:
+            heappush(self.heap, val)
+            heappop(self.heap)
+        else:
+            heappush(self.heap, val)
+
+        return self.heap[0]
         
 
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
