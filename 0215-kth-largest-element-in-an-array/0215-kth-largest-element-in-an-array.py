8d7705ef7f02d6f5c74073740526ff0534f64475
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-20 15:20:23 -0400

    Time: 94 ms (54.5%), Space: 31 MB (65.06%) - LeetHub - Time: O(n log k), Space: O(k)

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,40 +1,14 @@
+from collections import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # l = 0
-        # r = len(nums) - 1
-        # k = len(nums) - k 
-
-        # while 0 <= r < len (nums):
-        #     pivot = r
-        #     p = l
-        #     for i in range(l,r):
-        #         if nums[i] < nums[pivot]:
-        #             nums[i], nums[p] = nums[p], nums[i]
-        #             p += 1
-        #     nums[p],nums[r] = nums[r],nums[p]
-        #     if p == k: return nums[p]
-        #     if p < k: l = p + 1
-        #     if p > k: r = p - 1
-
-        # def quick_select(l,r):
-        #     pivot =  r
-        #     p = l
-        #     for i in range(l,r):
-        #         if nums[i] < nums[pivot]:
-        #             nums[i], nums[p] = nums[p], nums[i]
-        #             p += 1
-        #     nums[p],nums[r] = nums[r],nums[p]
-        #     if p == k: return nums[p]
-        #     if p < k: quick_select(p + 1,r)
-        #     if p > k: quick_select(l, p -1)
-        # return quick_select(l,r)
-
         heap = []
-
         for n in nums:
-            if len(heap) < k:
-                heapq.heappush(heap,n)
-            else:
-                heapq.heappushpop(heap,n)
+            heappush(heap, n)
+            if len(heap) > k:
+                heappop(heap)
         return heap[0]
+        
 
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
