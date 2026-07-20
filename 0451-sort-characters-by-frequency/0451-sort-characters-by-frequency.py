7ad5f386d5a991aeab75c6b07c6ae5da6e8dd6cd
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-20 15:59:16 -0400

    Time: 19 ms (20.97%), Space: 28.4 MB (8.71%) - LeetHub - Time: O(n), Space: O(n)

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,18 +1,21 @@
-import heapq
+from collections import Counter
+
 class Solution:
     def frequencySort(self, s: str) -> str:
-        map_c = defaultdict(int)
-        for n in s:
-            map_c[n] -= 1
+        freq_map = Counter(s)
+        buckets = [[] for _ in range(len(s) + 1)]
 
-        heap = [(f, c) for  c,f in map_c.items()]
-        # print(map_c,heap)
-        heapq.heapify(heap)
-        # print(heap)
-        s = ""
-        while heap:
-            (f, c) = heapq.heappop(heap)
-            # print(f,c)
-            s += -f*c
-        # print(s)
-        return s
+        for char, freq in freq_map.items():
+            buckets[freq].append(char)
+        
+        res = ""
+        for i in range(len(s), -1, -1):
+            if buckets[i]:
+                for char in buckets[i]:
+                    res += i * char
+        return res        
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
