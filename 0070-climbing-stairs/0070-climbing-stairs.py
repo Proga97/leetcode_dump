624a8e876dd2ec1ac15ddb6b4fcbc5cbfe528d8f
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-23 15:03:43 -0400

    Time: 0 ms (100%), Space: 19.3 MB (51.75%) - LeetHub - Time: O(n), Space: O(1)

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,14 +2,11 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 1:
             return 1
-
+            
         n1 = 1
         n2 = 2
-
         for i in range(3, n+1):
             count =  n1 + n2 
-            if i == n:
-                return count
             n1 = n2
             n2 = count
         
