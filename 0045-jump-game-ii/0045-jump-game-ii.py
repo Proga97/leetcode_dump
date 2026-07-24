d1f6309438cf150043697e1269c234df0f9cd617
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-24 12:04:11 -0400

    Time: 5 ms (61.72%), Space: 20 MB (92.61%) - LeetHub - Time: O(n), Space: O(1)

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,26 +1,43 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        dp = [-1] * len(nums)
-        dp[len(nums) - 1] = True
+        # dp = [-1] * len(nums)
+        # dp[len(nums) - 1] = True
 
-        def dfs(index):
-            if index >= len(nums) - 1:
-                return 0
-            if nums[index] == 0:
-                return math.inf
-            if dp[index] != -1:
-                return dp[index]
+        # def dfs(index):
+        #     if index >= len(nums) - 1:
+        #         return 0
+        #     if nums[index] == 0:
+        #         return math.inf
+        #     if dp[index] != -1:
+        #         return dp[index]
             
-            possible_jumps = min(index + nums[index], len(nums) - 1)
-            jumps = math.inf
-            for i in range(possible_jumps, index, -1):
-                jumps = min(jumps, dfs(i) + 1)
+        #     possible_jumps = min(index + nums[index], len(nums) - 1)
+        #     jumps = math.inf
+        #     for i in range(possible_jumps, index, -1):
+        #         jumps = min(jumps, dfs(i) + 1)
                 
-            dp[index] = jumps
-            return dp[index]
+        #     dp[index] = jumps
+        #     return dp[index]
 
         
-        return dfs(0)
+        # return dfs(0)
+
+        #greedy
+        curr_end, curr_far = 0, 0
+        n = len(nums) - 1
+        jumps = 0
+
+        for i in range(n):
+            curr_far = max(curr_far, i + nums[i])
+
+            if i == curr_end:
+                curr_end = curr_far
+                jumps += 1
+        
+        return jumps
+
+
+
 
 
 # Synced seamlessly with LeetHub Pro
