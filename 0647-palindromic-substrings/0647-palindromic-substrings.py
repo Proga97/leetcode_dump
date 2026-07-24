3601bdd90c9a8fd55ad45fe4291a34a76cd9db7a
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-24 17:16:15 -0400

    Time: 1531 ms (5.02%), Space: 321.5 MB (5.18%) - LeetHub - Time: O(n^2), Space: O(n^2)

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,20 +1,41 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         count = 0
-        for i in range((len(s))):
-            l,r = i,i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
+        dp = {}
+        # for i in range(len(s)):
+        #     dp[(i,i)] = True
+        #     count += 1
+
+        def dfs(l, r):
+            nonlocal count
+            if (l,r) in dp:
+                return dp[(l,r)]
+            if l > r:
+                # count += 1
+                dp[(l,r)] = True
+                return True
+            if l == r:
                 count += 1
-                l -= 1
-                r += 1
-            l,r = i,i+1
+                dp[(l,r)] = True
+                return True  
+            
+            if s[l] == s[r]:
+                if dfs(l+1, r-1):
+                    dp[(l,r)] = True
+                    count += 1
+                else:
+                    dp[(l,r)] = False
+            else:
+                dp[(l,r)] = False
 
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                count += 1
-                l -= 1
-                r += 1
+            dfs(l+1, r)
+            dfs(l, r-1)
+            return dp[(l,r)]  
 
-        # print(count)
-        return count 
-
+        dfs(0, len(s) - 1)
+        return count              
         
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
