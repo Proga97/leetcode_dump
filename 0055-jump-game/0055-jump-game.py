ab24a7e2ed81dc58151be9baccaaff77a1446c88
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-24 11:34:58 -0400

    Time: 15 ms (79.21%), Space: 20.2 MB (84.73%) - LeetHub - Time: O(n), Space: O(1)

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,25 +1,32 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        dp = [-1] * len(nums)
+        # dp = [-1] * len(nums)
 
-        def dfs(index):
-            if index >= len(nums) - 1:
-                # print("reached",index)
-                return True if index == len(nums) - 1 else False
-            if dp[index] != -1:
-                return dp[index]
+        # def dfs(index):
+        #     if index >= len(nums) - 1:
+        #         # print("reached",index)
+        #         return True if index == len(nums) - 1 else False
+        #     if dp[index] != -1:
+        #         return dp[index]
 
-            possible_jumps = min(index + nums[index], len(nums) - 1)
-            # print(index, possible_jumps)
-            for i in range(possible_jumps, index, -1):
-                if dfs(i):
-                    dp[i] = True
-                    return dp[i]
+        #     possible_jumps = min(index + nums[index], len(nums) - 1)
+        #     # print(index, possible_jumps)
+        #     for i in range(possible_jumps, index, -1):
+        #         if dfs(i):
+        #             dp[i] = True
+        #             return dp[i]
 
-            dp[index] = False
-            return dp[index]
+        #     dp[index] = False
+        #     return dp[index]
         
-        return dfs(0)
+        # return dfs(0)
+        
+        # greedy approach
+        lastPos = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= lastPos:
+                lastPos = i
+        return lastPos == 0
             
         
 
