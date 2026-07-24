1ebc1e4c9f196c83c9b4fd09f226cd0ed6529632
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-24 16:38:38 -0400

    Time: 291 ms (47.96%), Space: 19.2 MB (91.14%) - LeetHub - Time: O(n^2), Space: O(n)

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,28 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
-        resL = 0
+        res, resL = "", 0
 
         for i in range(len(s)):
+            # even length
             l, r = i, i
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l +1) > resL :
-                    resL = r - l +1
+                if r - l + 1 > resL:
                     res = s[l:r+1]
-                    # print("odd",resL,s[l:r+1],i,r,l)
+                    resL = r - l + 1
                 l -= 1
                 r += 1
-                
-            l, r = i, i+1
+            
+            # odd length
+            l, r = i, i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l +1) > resL :
-                    resL = r - l +1
+                if r - l + 1 > resL:
                     res = s[l:r+1]
-                    # print("even",resL,s[l:r+1],i,r,l)
+                    resL = r - l + 1
                 l -= 1
-                r += 1                
-        # print(res)
-        return res
-
-                
+                r += 1
         
+        return res
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
