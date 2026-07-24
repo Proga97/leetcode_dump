c03b6aafed8dbaefde521c9a823b1f43681a0c11
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-24 11:33:00 -0400

    Time: 5112 ms (5%), Space: 34.4 MB (5.07%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        dp = [-1] * len(nums)
+
+        def dfs(index):
+            if index >= len(nums) - 1:
+                # print("reached",index)
+                return True if index == len(nums) - 1 else False
+            if dp[index] != -1:
+                return dp[index]
+
+            possible_jumps = min(index + nums[index], len(nums) - 1)
+            # print(index, possible_jumps)
+            for i in range(possible_jumps, index, -1):
+                if dfs(i):
+                    dp[i] = True
+                    return dp[i]
+
+            dp[index] = False
+            return dp[index]
+        
+        return dfs(0)
+            
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
