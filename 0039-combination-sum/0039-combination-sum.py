a0fbaafab2b4e5fe7e746cb96e3c6f752e5fc3c6
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-26 20:30:43 -0400

    Time: 7 ms (76.8%), Space: 19.7 MB (8.8%) - LeetHub - Time: O(N^(T/M + 1)), Space: O(T/M)

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def dfs(i , path, total):
+            # if i >= len(candidates):
+            #     if total == target: res.append(path[:])
+            #     return
+            if total == target:
+                res.append(path[:])
+                return
+            
+            for j in range(i, len(candidates)):
+                if total + candidates[j] <= target:
+                    path.append(candidates[j])
+                    dfs(j, path, total + candidates[j])
+                    path.pop()
+            
+            # dfs(i + 1, path, total)
+            return 
+        
+        dfs(0, [], 0)
+        return res
+            
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
