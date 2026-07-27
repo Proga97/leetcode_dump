dc272a1db29367618f745f16635539bfc73bcb1a
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-27 01:13:56 -0400

    Time: 4701 ms (19.75%), Space: 19.5 MB (56.89%) - LeetHub - Time: O(, Space: O(?)

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,32 +1,33 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
-        path = set()
-
-        def dfs(r,c,i):
-            if i == len(word):
+        seen = set()
+        def dfs(i, j, index):
+            if index >= len(word):
                 return True
-            if 0 > r  or  rows <= r or 0 > c or c >= cols or (r,c) in path or board[r][c] != word[i]:
+
+            if (i, j) in seen or i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[index]:
+                # print("rej",i, j, index)
                 return False
-
-            directions = [[-1,0], [1,0], [0,-1], [0,1]]
-            path.add((r,c))
-            res = False
-            for dr, dc in directions:
-                newR = r + dr
-                newC = c + dc
-                res = res or dfs(newR,newC,i+1) 
-            path.remove((r,c))        
-            return res
+            # print(i, j, word[index])
+            seen.add((i, j))
+            if board[i][j] == word[index]:
+                up = dfs(i - 1, j, index + 1)
+                down = dfs(i + 1, j, index + 1)
+                right = dfs(i , j + 1, index + 1)
+                left = dfs(i , j - 1, index + 1)
+                if (up or down or right or left): 
+                    return True
+            seen.remove((i,j))
+            return False
         
-        for r in range(rows):
-            for c in range(cols):
-                if dfs(r,c,0): return True
-        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if dfs(i, j, 0):
+                    return True
         return False
-                    
-
-
-
 
         
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
