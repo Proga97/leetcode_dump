e561c51f13553abb65ec5a7d9b7e7a3c7ee2518a
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-28 18:07:46 -0400

    Time: 7 ms (44.19%), Space: 20.7 MB (49.95%) - LeetHub - Time: O(n + m), Space: O(n + m)

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        if numCourses < 1:
-            return False
-        preR = collections.defaultdict(list)
-        for c, r in  prerequisites:
-            preR[c].append(r)
-        # print(preR)
-
+        graph = defaultdict(list)
+        for out, inWard in prerequisites:
+            graph[out].append(inWard)
         seen = set()
-        def dfs(c):
-            if len(preR[c]) == 0: return True
-            if c in seen: return False 
+        for i in range(numCourses):
+            if not self.dfs(graph, i, seen):
+                return False
+        return True
+    
+    def dfs(self, graph, node, seen):
+        if not graph[node]: return True
+        if node in seen: return False
 
-            seen.add(c)
-            for r in preR[c]:
-                if not dfs(r): return False
-            seen.remove(c)
-            preR[c] = []
-            return True
-        
-        for c in range(numCourses):
-            if not dfs(c): return False
-        
+        seen.add(node)
+        for n in graph[node]:
+            if not self.dfs(graph, n, seen): return False
+        seen.remove(node)
+
+        graph[node] = []
         return True
 
-        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
