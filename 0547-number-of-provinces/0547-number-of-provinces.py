122f81186bdec8e7a05ed393c7f3668fa1b3a3eb
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-30 11:52:44 -0400

    Time: 11 ms (19.75%), Space: 20.4 MB (55.86%) - LeetHub - Time: O(n^2 * alpha(n)), Space: O(n)

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,22 +1,63 @@
-class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        seen = [False] * len(isConnected)
-        count = 0
-        stack = []
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+    
+    def find(self, n):
+        res = n
+        while res!= self.parent[res]:
+            self.parent[res] =  self.parent[self.parent[res]]
+            res = self.parent[res]
+        return res
+    
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
 
-        for i in range(len(isConnected)):
-            if not seen[i]:
-                stack.append(i)
-                count += 1  
-                while stack:
-                    n = stack.pop()
-                    for j in range(len(isConnected[n])):
-                        if isConnected[n][j] and not seen[j]:
-                            stack.append(j)
-                            seen[j] = True
+        if p1 == p2:
+            return 0
+        
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.parent[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        return 1
+            
+
+class Solution:
+    
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        # seen = [False] * len(isConnected)
+        # count = 0
+        # stack = []
+
+        # for i in range(len(isConnected)):
+        #     if not seen[i]:
+        #         stack.append(i)
+        #         count += 1  
+        #         while stack:
+        #             n = stack.pop()
+        #             for j in range(len(isConnected[n])):
+        #                 if isConnected[n][j] and not seen[j]:
+        #                     stack.append(j)
+        #                     seen[j] = True
 
                             
-        return count
+        # return count
+        n = len(isConnected)
+        uf = UnionFind(n)
+        res = n
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] == 1:
+                    res -= uf.union(i,j)
+        # print(uf.parent,res)
+        return res
+
+
+
+
 
 
 
