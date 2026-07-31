c9279374fc4bb6c5f3f361950da22c24bec05034
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-31 11:50:55 -0400

    Time: 56 ms (65.25%), Space: 20.2 MB (24.62%) - LeetHub - Time: O(log N), Space: O(N)

---
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -0,0 +1,20 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.calendar = SortedList()
+
+    def book(self, startTime: int, endTime: int) -> bool:
+        idx = self.calendar.bisect_right((startTime, endTime))
+        if (idx > 0 and self.calendar[idx-1][1] > startTime) or (idx < len(self.calendar) and self.calendar[idx][0] < endTime):
+            return False
+        self.calendar.add((startTime, endTime))
+        return True
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
