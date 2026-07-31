5a8cc220b60b1146abe4cf9bf3861d61cb463f42
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-07-31 12:59:00 -0400

    Time: 3 ms (21.76%), Space: 19.3 MB (56.88%) - LeetHub - Time: O(n), Space: O(1)

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
@@ -1,17 +1,29 @@
 class Solution:
     def findMiddleIndex(self, nums: List[int]) -> int:
-        prefix = [0] * len(nums)
-        suffix = [0] * len(nums)
-        for i in range(1, len(nums)):
-            prefix[i] = prefix[i -1] + nums[i-1]
-        # print(prefix)  
-        for i in range(len(nums) - 2, -1, -1):
-            suffix[i] = suffix[i + 1] + nums[i+1]
-        # print(suffix)
-        for i in range(0, len(nums)):
-            if prefix[i] == suffix[i]:
+        # prefix = [0] * len(nums)
+        # suffix = [0] * len(nums)
+        # for i in range(1, len(nums)):
+        #     prefix[i] = prefix[i -1] + nums[i-1]
+        # # print(prefix)  
+        # for i in range(len(nums) - 2, -1, -1):
+        #     suffix[i] = suffix[i + 1] + nums[i+1]
+        # # print(suffix)
+        # for i in range(0, len(nums)):
+        #     if prefix[i] == suffix[i]:
+        #         return i
+        # return -1     
+
+        totalSum = sum(nums)
+        leftSum = 0
+        for i in range(len(nums)):
+            
+            rightSum = totalSum - leftSum - nums[i]
+
+            if leftSum == rightSum:
                 return i
-        return -1     
+            leftSum += nums[i]
+            
+        return -1
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
