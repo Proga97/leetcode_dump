94982d724a59de90bfdc8abfc5e9e1b25ec062a4
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-08-03 12:08:47 -0400

    Time: 0 ms (100%), Space: 19.2 MB (71.97%) - LeetHub - Time: O(N), Space: O(N)

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,13 +6,19 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-
         if not p and not q:
-            return True 
-        if not p or not q or p.val != q.val:
+            return True
+
+        if not p or not q:
             return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+     
         
-        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
 
-
-
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
