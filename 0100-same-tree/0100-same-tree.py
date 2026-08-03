5ae47d6e6713b512ba3a324bb1fc3b7e9fe26d2b
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-08-03 12:19:33 -0400

    Time: 12 ms (0.25%), Space: 19.6 MB (6.54%) - LeetHub - Time: O(N), Space: O(N)

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -1,3 +1,5 @@
+import os
+import threading
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,7 +7,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.isSame = True
+
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        num_threads = os.cpu_count()
+        # print(num_threads)
+        return self.isSameMultiThread(p, q, num_threads)
+
+    def isSameMultiThread(self, p, q, num_threads):
         if not p and not q:
             return True
 
@@ -15,7 +25,22 @@ class Solution:
         if p.val != q.val:
             return False
 
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        if num_threads > 0:
+            def check_right_tree():
+                nonlocal p, q, num_threads
+                self.isSame &= self.isSameMultiThread(p.right, q.right, num_threads//2)
+            
+            t1 = threading.Thread(target = check_right_tree)
+            t1.start()
+
+            self.isSame &= self.isSameMultiThread(p.left, q.left, num_threads//2)
+
+            t1.join()
+        
+        else:
+            self.isSame &= self.isSameMultiThread(p.left, q.left, num_threads) and self.isSameMultiThread(p.right, q.right, num_threads)
+
+        return self.isSame
      
         
 
