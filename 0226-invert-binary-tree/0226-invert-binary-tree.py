366b1a92f00c464f6d6891bee98ab9d4df412557
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-08-03 12:55:52 -0400

    Time: 31 ms (0.05%), Space: 19.7 MB (20.91%) - LeetHub - Time: O(N), Space: O(N)

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -1,6 +1,5 @@
-from collections import deque
-
-
+import os
+import threading
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,38 +7,37 @@ from collections import deque
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.root = None
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        # Recursive method for inserting a value in the BST
-        # DFS
-        if not root:
-            return
-        # root.left, root.right = root.right, root.left
+        self.root = root
+        num_threads = os.cpu_count()
+        # print(num_threads)
+        return self.invert_tree_multi(root, num_threads)
 
-        # self.invertTree(root.left)
-        # self.invertTree(root.right)
-        # return root
+    def invert_tree_multi(self, node, num_threads):
+        if not node:
+            return 
+        node.left, node.right = node.right, node.left
+        if num_threads > 0:
+            def invert_right_tree():
+                nonlocal node, num_threads
+                self.invert_tree_multi(node.right, num_threads//2)
+            
+            t1 = threading.Thread(target = invert_right_tree)
+            t1.start()
 
-        # DFS Stack
-        # stack = [root]
-        # while stack:
-        #     node = stack.pop()
-        #     node.left, node.right = node.right, node.left
+            self.invert_tree_multi(node.left, num_threads//2)
 
-        #     if node.left:
-        #         stack.append(node.left)
-        #     if node.right:
-        #         stack.append(node.right)
-        # return root
+            t1.join()
+        else:
+            self.invert_tree_multi(node.right, num_threads) 
+            self.invert_tree_multi(node.left, num_threads)
 
-        # BFS Stack
-        queue = deque([root])
-        while queue:
-            node = queue.popleft()
-            node.left, node.right = node.right, node.left
+        return self.root
+     
 
-            if node.right:
-                queue.append(node.right)
-            if node.left:
-                queue.append(node.left)
 
-        return root
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
