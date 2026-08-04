b9c5d1efbeb6619640f28a0c84adebc092efe2b7
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-08-04 12:20:58 -0400

    Time: 107 ms (53.62%), Space: 19.8 MB (51.91%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        count_t = defaultdict(int)
+
+        for ch in t:
+            count_t[ch] += 1
+        
+        for ch in s:
+            count_t[ch] -= 1
+        
+        changes = 0
+        for count in count_t.values():
+            changes += abs(count)
+        
+        return changes // 2
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
