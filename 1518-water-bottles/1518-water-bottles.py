76f0c8564435e7b997c83588a679bcc6bf3c0602
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-08-04 18:30:31 -0400

    Time: 0 ms (100%), Space: 19.3 MB (19.03%) - LeetHub - Time: O(log(numBottles)), Space: O(1)

---
+++ 1518-water-bottles/1518-water-bottles.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        drank = 0
+        num_empty = 0
+        # prev = numBottles
+        emptyBottles = 0
+        while numBottles > 0:
+            drank += numBottles
+            emptyBottles += numBottles
+
+            numBottles = emptyBottles//numExchange 
+            emptyBottles =  emptyBottles % numExchange
+
+            # if prev == numBottles:
+            #     return drank
+            # prev = numBottles
+        return drank
+            
+
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
