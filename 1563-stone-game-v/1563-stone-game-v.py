aeb4d9797ddbf6be9d7bbeb0c7a229a981b14dbf
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-08-17 12:02:36 -0500

    Time: 8371 ms (27.84%), Space: 84.4 MB (32.16%) - LeetHub - Time: O(n^3), Space: O(n^2)

---
+++ 1563-stone-game-v/1563-stone-game-v.py
@@ -0,0 +1,28 @@
+class Solution:
+    def stoneGameV(self, stoneValue: List[int]) -> int:
+        @lru_cache(None) 
+        def dfs(left: int, right: int) -> int:
+            if left == right:
+                return 0
+
+            total = sum(stoneValue[left : right + 1])
+            suml = ans = 0
+            for i in range(left, right):
+                suml += stoneValue[i]
+                sumr = total - suml
+                if suml < sumr:
+                    ans = max(ans, dfs(left, i) + suml)
+                elif suml > sumr:
+                    ans = max(ans, dfs(i + 1, right) + sumr)
+                else:
+                    ans = max(ans, max(dfs(left, i), dfs(i + 1, right)) + suml)
+            return ans
+
+        n = len(stoneValue)
+        return dfs(0, n - 1)
+        
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
