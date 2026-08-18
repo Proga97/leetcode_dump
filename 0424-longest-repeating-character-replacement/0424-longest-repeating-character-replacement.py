8c21c72892b87b6ede84d5439a5cff37413ee964
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-08-18 13:08:10 -0500

    Time: 69 ms (95.02%), Space: 19.7 MB (51.61%) - LeetHub - Time: O(n), Space: O(1)

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,17 +1,21 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        size = 0 
-        map = collections.defaultdict(int)
+        freq = defaultdict(int)
         l = 0
-        maxf = 0 
+        res = 0
+        max_freq = 0
         for r in range(len(s)):
-            map[s[r]] = map[s[r]] + 1
-            maxf = max(maxf, map[s[r]])
-            while (r-l+1) - maxf > k :
-                map[s[l]] -= 1
+            freq[s[r]] += 1
+            max_freq = max(max_freq, freq[s[r]])
+            is_valid = (r - l + 1 - max_freq <= k)
+            if not is_valid:
+                freq[s[l]] -= 1
                 l += 1
-            size = max(r-l+1, size)
-        # print(map)
-        return size
 
-        
+            res = max(res, r - l + 1)
+
+        return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
