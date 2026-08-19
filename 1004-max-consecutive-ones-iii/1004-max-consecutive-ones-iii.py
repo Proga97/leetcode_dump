6a62714b47e3309b5c5996aced4f34225edacd39
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-08-19 13:47:01 -0500

    Time: 89 ms (7.56%), Space: 22.2 MB (22.46%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,19 +1,21 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        freq = defaultdict(int)
+        ones = 0
         l = 0
         res = 0
         for r in range(0, len(nums)):
-            freq[nums[r]] += 1
+            if nums[r] == 1: ones += 1
             
-            while r - l + 1 - freq[1] > k:
-                freq[nums[l]] -= 1
+            while r - l + 1 - ones > k:
+                if nums[l] == 1: ones -= 1
                 l += 1
             
             res = max(r - l + 1, res)
 
         return res
 
+                
+        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
