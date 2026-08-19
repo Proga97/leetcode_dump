f8ee864ca4ca34dbbde0d8c64d705239fce964a3
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-08-19 17:23:02 -0500

    Time: 8701 ms (6.38%), Space: 19.9 MB (70.52%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        w_length = len(words[0])
+        w_map = defaultdict(int)
+        for w in words:
+            w_map[w] += 1
+        # print(w_map)
+        req = len(words)
+        res = []
+        for i in range (len(s) - req * w_length + 1):
+            seen = defaultdict(int)
+            for j in range(i, i + req * w_length, w_length):
+                word = s[j:j+w_length]
+
+                if word not in w_map: break
+                
+                seen[word] += 1
+
+                if seen[word] > w_map[word]: break
+                # print(i, seen)
+                if seen == w_map: 
+                    res.append(i)
+        return res
+
+
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
