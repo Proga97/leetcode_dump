2e4ef65e0fc387e373463031f35dd5b19e9702a3
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-08-21 14:25:57 -0400

    Time: 5 ms (33.74%), Space: 21.5 MB (53.74%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -6,11 +6,12 @@ class Solution:
         rooms = []
         heapq.heappush(rooms,intervals[0][1])
         for i in range(1, len(intervals)):
-            if rooms[0] <= intervals[i][0]:
+            if rooms and intervals[i][0] >= rooms[0]:
                 heapq.heappop(rooms)
-            heapq.heappush(rooms,intervals[i][1])
-        return len(rooms)
+            heapq.heappush(rooms, intervals[i][1])
         
+        return len(rooms)
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
