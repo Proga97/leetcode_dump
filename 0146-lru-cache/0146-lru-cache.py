d6cd14e804ec3f7cb330f9e70d56ea2d7b0b50a8
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-08-31 11:09:47 -0400

    Time: 20 ms (94.81%), Space: 29.6 MB (77.73%) - LeetHub - Time: O(1), Space: O(capacity)

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,59 +1,61 @@
-class Node:
-    def __init__(self, key=0, val=0):
+class ListNode:
+    def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.prev = None
         self.next = None
+        self.prev = None
 
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
-        self.head = Node()
-        self.tail = Node()
-
+        self.hash = {}
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def _remove(self,node):
-        prev = node.prev
-        next = node.next
-        prev.next = next
-        next.prev = prev
-        self.cache.pop(node.key)
-    
-    def _insert(self,key,value):
-        next = self.head.next
-        node = Node(key,value)
-        self.cache[key] = node
-        self.head.next = node
-        node.prev = self.head
-        node.next = next
-        next.prev = node
-
     def get(self, key: int) -> int:
-        # print(self.cache,self.head)
-        if key in self.cache:
-            node = self.cache[key]
-            self._remove(node)
-            self._insert(node.key,node.val)
-            return node.val
-        return -1
+        if key not in self.hash:
+            return -1
+
+        node = self.hash[key]
+        self.remove(node)
+        self.add(node)
+
+        return node.val
 
     def put(self, key: int, value: int) -> None:
-        # print(self.cache,self.head)
-        if key in self.cache:
-            node = self.cache[key]
-            self._remove(node)
-        if len(self.cache) >= self.capacity:
-            self._remove(self.tail.prev)
-        self._insert(key,value)
+        if key in self.hash:
+            self.remove(self.hash[key])
         
+        node = ListNode(key, value)
+        self.hash[key] = node
+        self.add(node)
 
+        if len(self.hash) > self.capacity:
+            node_del = self.head.next
+            del self.hash[node_del.key]
+            self.remove(node_del)
 
+        
+    def add(self, node):
+        prev_node = self.tail.prev
+        prev_node.next = node
+        node.prev = prev_node
+        node.next = self.tail
+        self.tail.prev = node
+    
+    def remove(self, node):
+        prev_node = node.prev
+        prev_node.next = node.next
+        node.next.prev = prev_node
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
