4171abb14b0036441cdb1f73a909c52d96e838f6
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-09-07 12:09:07 -0400

    Time: 8 ms (73.44%), Space: 19.1 MB (91.67%) - LeetHub - Time: O(n), Space: O(1)

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
@@ -1,19 +1,24 @@
 class Solution:
     def distinctSubseqII(self, s: str) -> int:
         MOD = 10**9 + 7
-        dp = [1] # [""] -> ["", "a"] 
-        last = {}        
-        for i in range(len(s)):
-            dp.append(dp[-1] * 2)
-            if s[i] in last:
-                dp[-1] -= dp[last[s[i]]]
-            last[s[i]] = i
+        # dp = [1] # [""] -> ["", "a"] 
+        # last = {}        
+        # for i in range(len(s)):
+        #     dp.append(dp[-1] * 2)
+        #     if s[i] in last:
+        #         dp[-1] -= dp[last[s[i]]]
+        #     last[s[i]] = i
 
-        return (dp[-1] - 1) % MOD
+        # return (dp[-1] - 1) % MOD
 
+        res = 0
+        dp = defaultdict(int)
+        for c in s:
+            new = res + 1 - dp[c]
+            res = (res + new) % MOD
+            dp[c] += new
 
-
-        
+        return res % MOD
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
