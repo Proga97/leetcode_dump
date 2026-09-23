102636e750641eb66fe79be85631ea635393054c
Pranay Chimmani <39213870+Proga97@users.noreply.github.com>
2026-09-23 14:02:20 -0400

    Time: 108 ms (32.79%), Space: 30.8 MB (81.29%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minOperations(self, nums: list[int], x: int) -> int:
+        total = sum(nums)
+        n = len(nums)
+        maxi = -1
+        left = 0
+        current = 0
+
+        for right in range(n):
+            # sum([left ,..., right]) = total - x
+            current += nums[right]
+            # if larger, move `left` to left
+            while current > total-x and left <= right:
+                current -= nums[left]
+                left += 1
+            # check if equal
+            if current == total-x:
+                maxi = max(maxi, right-left+1)
+
+        return n-maxi if maxi != -1 else -1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
